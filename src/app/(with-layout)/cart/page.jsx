aebc61e31ea2868b20242b9cbc55c2dbffd36ef6
unknown <imran.k.professional@gmail.com>
2023-07-28 01:21:29 +0600

    completed demo responsive card for cart

--- src/app/(with-layout)/cart/page.jsx
+++ src/app/(with-layout)/cart/page.jsx
@@ -39,9 +39,11 @@ const Summary = () => {
       <h2 className="text-lg font-medium text-gray-900">Order summary</h2>
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-          <div className="text-base font-medium text-gray-900">Order total</div>
+          <div className="text-base font-semibold text-gray-900">
+            Grand total
+          </div>
           {/* <Currency value={totalPrice} /> */}
-          2333
+          <span className="font-bold">$2333</span>
         </div>
       </div>
       <div className="flex flex-1 w-full items-center justify-center">
@@ -69,152 +71,116 @@ function cart() {
                   <CartItem key={item.id} data={item} />
                 ))} */}
               {/* <CartItem key={item.id} data={item} /> */}
-              <>
-                <li className="flex py-6 border-b">
-                  <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
-                    <Image
-                      fill
-                      src={
-                        "https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                      }
-                      alt="kjnf"
-                      className="object-cover object-center"
-                    />
-                  </div>
-                  <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                    <div className="absolute z-10 right-0 top-0">
-                      {/* <IconButton onClick={onRemove} icon={<X size={15} />} /> */}
-                      X
-                    </div>
-                    <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                      <div className="flex flex-col justify-between">
-                        <p className=" text-lg font-semibold text-black">
-                          Apple
-                        </p>
-                        <p>in laptops</p>
-                        {/* <p>Price</p> */}
-                        <div className="flex gap-2">
-                          <p className="text-black font-semibold">Quantity: </p>
-                          <div className="flex gap-2">
-                            <button className="border text-sm p-1 rounded-full border-gray-500 cursor-pointer inline-flex items-center no-underline font-medium">
-                              <HiMinus />
-                            </button>
-                            <div className="border font-semibold px-3 flex items-center">
-                              2
-                            </div>
-                            <button className="border text-sm rounded-full p-1 border-gray-500  cursor-pointer inline-flex items-center no-underline font-medium">
-                              <HiPlus />
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="mt-1 flex flex-col text-sm">
-                        <p className="text-gray-500">Quantity</p>
-                        <div className="flex gap-2">
-                          <button className="border text-lg p-1 rounded-full border-gray-500 cursor-pointer inline-flex items-center no-underline font-medium">
-                            <HiMinus />
-                          </button>
-                          <div className="border font-semibold px-3 flex items-center">
-                            2
-                          </div>
-                          <button className="border text-lg rounded-full p-1 border-gray-500  cursor-pointer inline-flex items-center no-underline font-medium">
-                            <HiPlus />
-                          </button>
-                        </div>
-                        {/* <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">
-                          size
-                        </p> */}
-                      </div>
-                      {/* <Currency value={data.price} /> */}
-                    </div>
-                  </div>
-                </li>
-              </>
-              <>
-                <li className="flex py-6 border-b">
-                  <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
-                    <Image
-                      fill
-                      src={
-                        "https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                      }
-                      alt="kjnf"
-                      className="object-cover object-center"
-                    />
-                  </div>
-                  <div className="flex flex-1 justify-start pl-6">
-                    <div className="flex flex-col gap-1">
-                      <p className="font-semibold text-lg">Apple</p>
-                      <p className="text-gray-500">— in phones</p>
-                      <div className="flex gap-2">
-                        <span className="font-medium">Qty: </span>
-                        <div
-                          className="flex flex-1 max-w-[100px] items-center
+
+              <li className="flex py-6 border-b">
+                <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
+                  <Image
+                    fill
+                    src={
+                      "https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+                    }
+                    alt="kjnf"
+                    className="object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-1 justify-start pl-6">
+                  <div className="flex flex-col gap-1">
+                    <p className="font-semibold text-lg">Apple</p>
+                    <p className="text-gray-500">— in phones</p>
+                    <div className="flex gap-2">
+                      <span className="font-medium">Qty: </span>
+                      <div
+                        className="flex flex-1 max-w-[100px] items-center
                       h-full border text-black font-medium
                       "
-                        >
-                          <div
-                            className="flex flex-1 justify-center
+                      >
+                        <div
+                          className="flex flex-1 justify-center
                         items-center cursor-pointer
                         "
-                          >
-                            <HiMinus />
-                          </div>
-                          <div className="h-full flex justify-center items-center px-2">
-                            <span>2</span>
-                          </div>
-                          <div className="flex flex-1 h-full justify-center items-center cursor-pointer">
-                            <HiPlus />
-                          </div>
+                        >
+                          <HiMinus />
+                        </div>
+                        <div className="h-full flex justify-center items-center px-2">
+                          <span>2</span>
+                        </div>
+                        <div className="flex flex-1 h-full justify-center items-center cursor-pointer">
+                          <HiPlus />
                         </div>
                       </div>
-                      <p className="font-bold text-lg">Price: $40</p>
                     </div>
+                    <p className="font-bold text-lg">Price: $40</p>
                   </div>
-                  <div className="flex relative flex-col justify-end">
-                    <span className="absolute text-lg top-0 right-0 hover:text-red-600">
-                      <HiOutlineTrash />
-                    </span>
-                    <p className="text-gray-900 text-lg font-semibold">
-                      Subtotal : $11
+                </div>
+                <div className="flex relative flex-col justify-end">
+                  <span className="absolute text-lg top-0 right-0 hover:text-red-600">
+                    <HiOutlineTrash />
+                  </span>
+                  <p className="text-gray-900 text-lg font-semibold">
+                    Subtotal : $11
+                  </p>
+                </div>
+              </li>
+              <li className="flex py-6 border-b">
+                <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
+                  <Image
+                    fill
+                    src={
+                      "https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+                    }
+                    alt="kjnf"
+                    className="object-cover object-center"
+                  />
+                </div>
+                <div className="flex flex-1 justify-start pl-6 max-[320px]:pl-4">
+                  <div className="flex flex-col gap-1">
+                    <p className="font-semibold text-lg max-[320px]:text-base">
+                      Apple
+                    </p>
+                    <p className="text-gray-500 max-[320px]:text-sm">
+                      — in phones
+                    </p>
+                    <div className="flex gap-2">
+                      <span className="font-medium max-[320px]:text-sm">
+                        Qty:{" "}
+                      </span>
+                      <div
+                        className="flex flex-1 max-w-[100px] items-center
+                      h-full border text-black font-medium
+                      "
+                      >
+                        <div
+                          className="flex flex-1 justify-center
+                        items-center cursor-pointer max-[320px]:text-sm
+                        "
+                        >
+                          <HiMinus />
+                        </div>
+                        <div className="h-full flex justify-center items-center px-2 max-[320px]:px-1 max-[320px]:text-sm">
+                          <span>2</span>
+                        </div>
+                        <div className="flex flex-1 h-full justify-center items-center cursor-pointer max-[320px]:text-sm">
+                          <HiPlus />
+                        </div>
+                      </div>
+                    </div>
+                    <p className="font-bold text-lg max-[320px]:text-[13px]">
+                      Price: $4000
                     </p>
                   </div>
-                </li>
-                {/* </>
-              <> */}
-                <li className="flex py-6 border-b">
-                  <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
-                    <Image
-                      fill
-                      src={
-                        "https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                      }
-                      alt="kjnf"
-                      className="object-cover object-center"
-                    />
-                  </div>
-                  <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                    <div className="absolute z-10 right-0 top-0">
-                      {/* <IconButton onClick={onRemove} icon={<X size={15} />} /> */}
-                      X
-                    </div>
-                    <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                      <div className="flex justify-between">
-                        <p className=" text-lg font-semibold text-black">
-                          name
-                        </p>
-                      </div>
-                      <div className="mt-1 flex text-sm">
-                        <p className="text-gray-500">color</p>
-                        <p className="ml-4 border-l border-gray-200 pl-4 text-gray-500">
-                          size
-                        </p>
-                      </div>
-                      {/* <Currency value={data.price} /> */}4477
-                    </div>
-                  </div>
-                </li>
-              </>
+                </div>
+                <div className="flex relative flex-col justify-end">
+                  <span className="absolute text-lg top-0 right-0 hover:text-red-600">
+                    <HiOutlineTrash />
+                  </span>
+                  <p className="text-gray-900 text-lg font-semibold max-[320px]:text-xs max-[320px]:pb-1.5 max-[330px]:hidden">
+                    total : $1100
+                  </p>
+                  <p className="text-gray-900 text-lg font-semibold max-[320px]:text-xs max-[320px]:pb-1.5 min-[331px]:hidden">
+                    total : $1100
+                  </p>
+                </div>
+              </li>
             </ul>
           </div>
           <Summary />
