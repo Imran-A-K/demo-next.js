ae83ecc4bc978398a95edafd6772d0d647600fca
unknown <imran.k.professional@gmail.com>
2023-07-28 15:47:53 +0600

    buggy code backup

--- src/app/components/Cart/CartCard/page.js
+++ src/app/components/Cart/CartCard/page.js
@@ -15,7 +15,7 @@ function CartCard({ item }) {
   } = item;
   return (
     <>
-      <li className="flex py-6 border-b">
+      <li className=" flex py-6 border-b">
         <div className="relative flex flex-col gap-2 h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
           <Image
             width={100}
@@ -27,6 +27,7 @@ function CartCard({ item }) {
             alt="kjnf"
             className="h-3/4 w-full  object-center rounded-b-md object-scale-down"
           />
+
           <div className="flex justify-center">
             <svg
               className="fill-current text-gray-700 w-3 "
@@ -35,7 +36,7 @@ function CartCard({ item }) {
               <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
             </svg>
 
-            <span className="mx-2 border text-center w-8 max-[320px]:text-sm max-[320px]:w-6 rounded-lg">
+            <span className="mx-2 border text-center w-8 max-[320px]:text-sm max-[320px]:w-6 rounded-lg z-30">
               {quantity}
             </span>
 
@@ -119,6 +120,81 @@ function CartCard({ item }) {
 }
 
 export default CartCard;
+{
+  // <li className="flex py-6 border-b min-[641px]:hidden">
+  //   <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
+  //     <Image
+  //       fill
+  //       src={
+  //         images[0] ||
+  //         "https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
+  //       }
+  //       alt="kjnf"
+  //       className="object-cover object-center"
+  //     />
+  //   </div>
+  //   <div className="flex flex-1 justify-start pl-6 max-[320px]:pl-4">
+  //     <div className="flex flex-col gap-1">
+  //       <p className="font-semibold text-lg max-[320px]:text-base">
+  //         {title}
+  //       </p>
+  //       <p className="text-gray-500 max-[320px]:text-sm">
+  //         — from <span className="">{brand}</span>
+  //       </p>
+  //       <div className="flex gap-2">
+  //         <span className="font-medium max-[320px]:text-sm">Qty: </span>
+  //         {/* <div
+  //           className="flex flex-1 max-w-[100px] items-center
+  //               h-full border text-black font-medium
+  //               "
+  //         >
+  //           <div
+  //             className="flex flex-1 justify-center
+  //                 items-center cursor-pointer max-[320px]:text-sm
+  //                 "
+  //           >
+  //             <HiMinus />
+  //           </div>
+  //           <div className="h-full flex justify-center items-center px-2 max-[320px]:px-1 max-[320px]:text-sm">
+  //             <span>2</span>
+  //           </div>
+  //           <div className="flex flex-1 h-full justify-center items-center cursor-pointer max-[320px]:text-sm">
+  //             <HiPlus />
+  //           </div>
+  //         </div> */}
+  //         <div className="flex justify-center">
+  //           <svg
+  //             className="fill-current text-gray-700 w-3 "
+  //             viewBox="0 0 448 512"
+  //           >
+  //             <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+  //           </svg>
+  //           <span className="mx-2 border text-center w-8 max-[320px]:text-sm max-[320px]:w-6 rounded-lg">
+  //             {quantity}
+  //           </span>
+  //           <svg
+  //             className="fill-current text-gray-600 w-3"
+  //             viewBox="0 0 448 512"
+  //           >
+  //             <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+  //           </svg>
+  //         </div>
+  //       </div>
+  //       <p className="font-bold text-lg max-[320px]:text-[13px]">
+  //         Price: ${price}
+  //       </p>
+  //     </div>
+  //   </div>
+  //   <div className="flex relative flex-col justify-end">
+  //     <span className="absolute text-lg top-0 right-0 hover:text-red-600">
+  //       <HiOutlineTrash />
+  //     </span>
+  //     <p className="text-gray-800 text-lg font-semibold max-[320px]:text-xs max-[320px]:pb-1.5">
+  //       Total : ${quantity * price}
+  //     </p>
+  //   </div>
+  // </li>
+}
 {
   /* <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-28 sm:w-28">
--- src/app/hooks/cartFunctions/cartFunctions.js
+++ src/app/hooks/cartFunctions/cartFunctions.js
@@ -1,19 +1,36 @@
 export default function addToCart(product) {
-  // Check if there's any cart field array set in local storage
   const cartItems = JSON.parse(localStorage.getItem("cart")) || [];
 
-  // Check if the product is already in the cart by checking its ID
   const existingProduct = cartItems.find((item) => item.id === product.id);
 
   if (existingProduct) {
-    // If the product is already in the cart, do nothing
     return;
   } else {
-    // If the product is not in the cart, add it with quantity 1
     product.quantity = 1;
     cartItems.push(product);
   }
 
-  // Save the updated cart array back to local storage
   localStorage.setItem("cart", JSON.stringify(cartItems));
+}
+
+export function updateCartItemQuantity(itemId, increment) {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  const itemIndex = cart.findIndex((item) => item.id === itemId);
+
+  if (itemIndex !== -1) {
+    if (increment) {
+      cart[itemIndex].quantity += 1;
+    } else {
+      if (cart[itemIndex].quantity > 0) {
+        cart[itemIndex].quantity -= 1;
+      }
+    }
+  } else {
+    if (increment) {
+      cart.push({ id: itemId, quantity: 1 });
+    }
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
